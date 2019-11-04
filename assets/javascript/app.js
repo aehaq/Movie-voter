e38c2a809edbdb9c81a09f76464d2c2f9bc43ce6
aehaq <azfarehaq@gmail.com>
2019-11-03 19:01:43 -0800

    updated everything except the voting function and database used

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+$(document).ready(function() {
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyAMrOHyzkp35RwzUzl-s5hgjMkoZJwiyhw",
@@ -8,47 +9,17 @@ var config = {
     messagingSenderId: "1018954509950"
 };
 firebase.initializeApp(config);
-
 var database = firebase.database();
 
-// This function converts arrival to be moment.js compatible, and ensures it uses today's date.
-function firstArrival(initialVal) {
-    var firstArrival = moment(initialVal, "hh:mm a")
-    return firstArrival
-};
-
-// This function calculates when the nextArrival will be based off of the first arrival and the frequency of the train.
-function nextArrival(firstArrival, interval) {
-    
-    // Gives the next arrival an initial value equal to the first arrival
-    var nextArrival = firstArrival;
-    
-    // For every time that the train's next arrival has already occured, we move the next arrival up by the train's scheduled frequency.
-    // This stops when the next arrival actually occurs after the current time.
-    while (moment().diff(nextArrival, 'minute') > 0) {
-        nextArrival = nextArrival.add(interval, 'minute') 
-    }
-    return nextArrival;
-};
-
-// Calculates time until next train's arrival, by comparing the arrival time to the current time.
-function timeUntil(nextArrival) {
-    var timeUntil = moment().diff(nextArrival, 'minute') * -1
-    return timeUntil;
-};
 
 // This function takes a train listed in the database, and prints it's associated info onto the table.
-function listTrains(trainSnapshot) {
-    var name = trainSnapshot.name;
-    var dest = trainSnapshot.destination;
-    var original = trainSnapshot.originalTrain;
-    var int = trainSnapshot.interval;
+function listMovies(movieSnapshot) {
+    var name = movieSnapshot.name;
+    var genre = movieSnapshot.genre;
+    var sellingPoint = movieSnapshot.sellingPoint;
+    var votes = movieSnapshot.votes;
     
-    //  Rather than store our calculations onto firebase, we run the necessary functions after retrieving the original information from the database.
-    var firstTrain = firstArrival(original)
-    var nextTrain = nextArrival(firstTrain, int);
-    var timeLeft = timeUntil(nextTrain);
-    
+    console.log(movieSnapshot)
     // We create a new row to append the information associated wth the current .
     var newRow = $('<tr>')
 
@@ -57,24 +28,26 @@ function listTrains(trainSnapshot) {
     nameCell.text(name);
     newRow.append(nameCell);
     
-    var destCell = $('<td scope="row">')
-    destCell.text(dest);
-    newRow.append(destCell);
+    var genreCell = $('<td scope="row">')
+    genreCell.text(genre);
+    newRow.append(genreCell);
     
-    var freqCell = $('<td scope="row">')
-    freqCell.text(int);
-    newRow.append(freqCell);
+    var sellCell = $('<td scope="row">')
+    sellCell.text(sellingPoint);
+    newRow.append(sellCell);
+
+    var votesCell = $('<td scope="row">')
+    votesCell.text(votes);
+    newRow.append(votesCell);
     
-    var nextCell = $('<td scope="row">')
-    nextCell.text(nextTrain.format("HH:mm"));
-    newRow.append(nextCell);
-    
-    var awayCell = $('<td scope="row">')
-    awayCell.text(timeLeft + " min");
-    newRow.append(awayCell);
+    var castCell = $('<td scope="row">')
+    var button = $(`<button class="btn btn-success" data-key=${movieSnapshot.key}>`)
+    button.text("VOTE!")
+    castCell.append(button)
+    newRow.append(castCell);
 
     // We append the new row to the table.   
-    $('#trains-here').append(newRow);
+    $('#movies-here').append(newRow);
 };
 
 // This function is used to render the list of trains without making changes to firebase
@@ -84,24 +57,23 @@ function render() {
     database.ref().once("value", function(snapshot){
 
         // We must empty the table out since we will work through the entire database again.
-        $('#trains-here').empty();
+        $('#movies-here').empty();
 
         // Here we narrow our scope to an object containing all of the trains.
-        var trainsObject = snapshot.val();
+        var moviesObject = snapshot.val();
 
         // We then iterate through the trains on the list and add each batch of information to the table
-        for (key in trainsObject) {
-            listTrains(trainsObject[key]);
+        for (key in moviesObject) {
+            listMovies(moviesObject[key]);
         }
     });
 }
 
-$(document).ready(function() {
     
     // This function calls the list train function when a change is made to the database.
     // When the page loads, this function is run once automatically, and filters through every single train as though it was newly added. 
     database.ref().on("child_added", function(snapshot) {
-        listTrains(snapshot.val());
+        listMovies(snapshot.val());
     });
 
     // This function is run when users want to add train information to the list.
@@ -111,19 +83,19 @@ $(document).ready(function() {
         
         // Here we grab the user data from the forms on page and stores them as variables.
         var name = $('#name-input').val();
-        var dest = $('#dest-input').val();
-        var initTrain = $('#initial-input').val();
-        var frequency = $('#freq-input').val();
+        var genre = $('#genre-input').val();
+        var sellingPoint = $('#sell-input').val();
 
         // This if statement ensures that none of the forms were left empty.        
-        if (name != "" && dest != "" && initTrain != "" && frequency != "") {
+        if (name != "" && genre != "" && sellingPoint != "") {
             
             //If none of the forms were empty, the data is pushed to firebase.
             database.ref().push({
                 name: name,
-                destination: dest,
-                originalTrain: initTrain,
-                interval: frequency
+                genre: genre,
+                sellingPoint: sellingPoint,
+                votes: 0,
+                id: Math.floor(Math.random()*100000)
             });
             
             $('.form-control').val('');
