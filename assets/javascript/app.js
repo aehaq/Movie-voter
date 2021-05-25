2f630404723e37799144e03c57d54f6df8dfa3d8
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-05-24 19:58:36 -0700

    replace firebase with hoodie (dev version)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,14 @@
-$(document).ready(function() {
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyAMrOHyzkp35RwzUzl-s5hgjMkoZJwiyhw",
-    authDomain: "database-434b0.firebaseapp.com",
-    databaseURL: "https://database-434b0.firebaseio.com",
-    projectId: "database-434b0",
-    storageBucket: "database-434b0.appspot.com",
-    messagingSenderId: "1018954509950"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
+function loadAndRenderItems() {
+    hoodie.store.findAll().then(render)
+}
 
+/* render items initially on page load */
+loadAndRenderItems()
+
+hoodie.store.on('change', loadAndRenderItems)
+hoodie.store.on('clear', function () {
+    render([])
+})
 
 // This function takes a movie listed in the database, and prints it's associated info onto the table.
 function listMovies(movieSnapshot) {
@@ -18,7 +16,7 @@ function listMovies(movieSnapshot) {
     var genre = movieSnapshot.genre;
     var sellingPoint = movieSnapshot.sellingPoint;
     var votes = movieSnapshot.votes;
-    var id = movieSnapshot.id;
+    var id = movieSnapshot._id;
 
     // We create a new row to append the information associated wth the current .
     var newRow = $('<tr>')
@@ -51,28 +49,20 @@ function listMovies(movieSnapshot) {
 };
 
 // This function is used to render the list of movies without making changes to firebase
-function render() {
+function render(moviesObject) {
+    // We must empty the table out since we will work through the entire database again.
+    $('#movies-here').empty();
 
-    // This rerenders the information in the database and takes a snapshot of the whole thing as an object.
-    database.ref().once("value", function(snapshot){
-
-        // We must empty the table out since we will work through the entire database again.
-        $('#movies-here').empty();
-
-        // Here we narrow our scope to an object containing all of the movies.
-        var moviesObject = snapshot.val();
-
-        // We then iterate through the movies on the list and add each batch of information to the table
-        for (key in moviesObject) {
-            listMovies(moviesObject[key]);
-        }
-    });
+    // We then iterate through the movies on the list and add each batch of information to the table
+    for (key in moviesObject) {
+        listMovies(moviesObject[key]);
+    }
 }
 
     
     // This function calls the list movie function when a change is made to the database.
     // When the page loads, this function is run once automatically, and filters through every single movie as though it was newly added. 
-    database.ref().on("child_added", function(snapshot) {
+    hoodie.on("child_added", function(snapshot) {
         listMovies(snapshot.val());
     });
 
@@ -90,7 +80,7 @@ function render() {
         if (name != "" && genre != "" && sellingPoint != "") {
             
             //If none of the forms were empty, the data is pushed to firebase.
-            database.ref().push({
+            hoodie.store.add({
                 name: name,
                 genre: genre,
                 sellingPoint: sellingPoint,
@@ -106,26 +96,7 @@ function render() {
         event.preventDefault();
 
         var key = $(this).data("key")
-        dbItem = database.ref().orderByChild("id").equalTo(key);
-        dbItem.once("value", function(snapshot){
-            var snapshotObject = snapshot.val()
-            for (key in snapshotObject) {
-                let movie = snapshotObject[key];
-                let newVote = movie.votes + 1;
-                console.log(movie.votes)
-                database.ref().child(key).set({
-                    name: movie.name,
-                    genre: movie.genre,
-                    sellingPoint: movie.sellingPoint,
-                    votes: newVote,
-                    id: movie.id
-                })
-            }
-            render()
+        hoodie.store.update(key, (value) => {
+            value.votes += 1
         })
     })
-
-    // In order to keep the movie schedule up to date, we run the render function every 10 seconds.
-    // Because the calculations happen every time the table is rendered, the minutes left and next arrival will change whenever necessary
-    setInterval(render, 10000);
-});
